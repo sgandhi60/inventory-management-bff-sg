1a6eb330bc13063490bd698bd958315290131b90
Shailesh Gandhi <shailesh@Shaileshs-MacBook-Pro.local>
2020-09-01 14:26:59 -0400

    Adds a mock service implementation

--- test/controllers/stock-items.controller.spec.ts
+++ test/controllers/stock-items.controller.spec.ts
@@ -1,16 +1,29 @@
 import {Application} from 'express';
 import * as request from 'supertest';
+import {Container} from 'typescript-ioc';
 
 import {buildApiServer} from '../helper';
+import Mock = jest.Mock;
+import {StockItemsMockService} from '../../src/services';
 
 describe('stock-item.controller', () => {
 
   let app: Application;
-  beforeEach(async () => {
+  let service_listStockItems: Mock;
+
+   beforeEach(async () => {
+    service_listStockItems = jest.fn();
+    Container.bind(StockItemsMockService).factory(
+      () => ({
+        listStockItems: service_listStockItems
+      }),
+    );
+
     const apiServer = buildApiServer();
 
     app = await apiServer.getApp();
   });
+
 
   test('canary verifies test infrastructure', () => {
      expect(true).toEqual(true);
@@ -18,12 +31,27 @@ describe('stock-item.controller', () => {
 
   describe('given GET /stock-items', () => {
     describe('when service is successful', () => {
+      const expectedResult = [{value: 'val'}];
+      beforeEach(() => {
+        service_listStockItems.mockResolvedValue(expectedResult);
+      });
+
       test('then return 200 status', async () => {
         return request(app).get('/stock-items').expect(200);
       });
 
-      test('then should return an empty array', async () => {
-        return request(app).get('/stock-items').expect([]);
+      test('then should return value from service', async () => {
+        return request(app).get('/stock-items').expect(expectedResult);
+      });
+    });
+
+    describe('when service fails', () => {
+      beforeEach(() => {
+        service_listStockItems.mockRejectedValue(new Error('service failed'));
+      });
+
+      test('then return 502 error', async () => {
+        return request(app).get('/stock-items').expect(502);
       });
     });
   });
